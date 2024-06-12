492032701f2bcb08cbce78fb36d6e654e34edb52
Henrik Irgens Gravdal <47174810+henrikig@users.noreply.github.com>
2024-06-12 23:12:33 +0200

    feat: trouble and tmux navigator

---
+++ lua/custom/plugins/trouble.lua
@@ -0,0 +1,24 @@
+return {
+  'folke/trouble.nvim',
+  opts = {
+    focus = true,
+  },
+  cmd = 'Trouble',
+  keys = {
+    {
+      '<leader>xx',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xX',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = 'Buffer Diagnostics (Trouble)',
+    },
+    {
+      '<leader>cs',
+      '<cmd>Trouble symbols toggle focus=false<cr>',
+      desc = 'Symbols (Trouble)',
+    },
+  },
+}
